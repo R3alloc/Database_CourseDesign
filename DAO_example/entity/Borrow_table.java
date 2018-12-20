f4dbbeb7fb570bdbcf54282cc9c0c858a9fa65fd
Fande Yu <yu5389522@126.com>
2018-12-20 21:28:42 +0800

    借阅关系表

---
+++ DAO_example/entity/Borrow_table.java
@@ -0,0 +1,53 @@
+import java.sql.Date;
+
+public class Borrow_table
+{
+    //条码号和用户id的组合 组成一条借书记录的主键
+    private String loc_barcode;	//条码号
+    private String user_id;	//用户id
+
+    private String book_name;
+    private Date loc_borrTime;
+    private Date loc_dueTime;
+
+    public String getLoc_barcode() {
+    	return loc_barcode;
+    }
+    
+    public void setLoc_barcode(String loc_barcode) {
+    	this.loc_barcode = loc_barcode;
+    }
+
+    public String getLoc_room() {
+    	return loc_room;
+    }
+    
+    public void setLoc_room(String loc_room) {
+    	this.loc_room = loc_room;
+    }   
+    
+    public Date getLoc_borrTime() {
+    	return loc_borrTime;
+    }
+    
+    public void setLoc_borrTime(Date loc_borrTime) {
+    	this.loc_borrTime = loc_borrTime;
+    }
+    
+    public Date getLoc_dueTime() {
+    	return loc_dueTime;
+    }
+    
+    public void setLoc_dueTime(Date loc_dueTime) {
+    	this.loc_dueTime = loc_dueTime;
+    }		
+    
+    public String getUser_id() {
+    	return user_id;
+    }
+    
+    public void setUser_id(String user_id) {
+    	this.user_id = user_id;
+    }
+
+}
